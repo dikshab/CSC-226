088cc687c9ea72816dbb769e0bf6e09d1cfdc927
Diksha Bansal <dikshab@uvic.ca>
2018-06-27 18:52:47 -0700

    files updated

---
+++ Assignments/Assignment2/PrimVsKruskal.java
@@ -0,0 +1,184 @@
+/* PrimVsKruskal.java
+   CSC 226 - Summer 2018
+   Assignment 2 - Prim MST versus Kruskal MST Template
+   
+   The file includes the "import edu.princeton.cs.algs4.*;" so that yo can use
+   any of the code in the algs4.jar file. You should be able to compile your program
+   with the command
+   
+	javac -cp .;algs4.jar PrimVsKruskal.java
+	
+   To conveniently test the algorithm with a large input, create a text file
+   containing a test graphs (in the format described below) and run
+   the program with
+   
+	java -cp .;algs4.jar PrimVsKruskal file.txt
+	
+   where file.txt is replaced by the name of the text file.
+   
+   The input consists of a graph (as an adjacency matrix) in the following format:
+   
+    <number of vertices>
+	<adjacency matrix row 1>
+	...
+	<adjacency matrix row n>
+	
+   Entry G[i][j] >= 0.0 of the adjacency matrix gives the weight (as type double) of the edge from 
+   vertex i to vertex j (if G[i][j] is 0.0, then the edge does not exist).
+   Note that since the graph is undirected, it is assumed that G[i][j]
+   is always equal to G[j][i].
+
+
+   R. Little - 06/22/2018
+*/
+
+ import edu.princeton.cs.algs4.*;
+ import java.util.Scanner;
+ import java.io.File;
+
+//Do not change the name of the PrimVsKruskal class
+public class PrimVsKruskal{
+
+	/* PrimVsKruskal(G)
+		Given an adjacency matrix for connected graph G, with no self-loops or parallel edges,
+		determine if the minimum spanning tree of G found by Prim's algorithm is equal to 
+		the minimum spanning tree of G found by Kruskal's algorithm.
+		
+		If G[i][j] == 0.0, there is no edge between vertex i and vertex j
+		If G[i][j] > 0.0, there is an edge between vertices i and j, and the
+		value of G[i][j] gives the weight of the edge.
+		No entries of G will be negative.
+	*/
+	static boolean PrimVsKruskal(double[][] G){
+		int n = G.length;
+
+		/* Build the MST by Prim's and the MST by Kruskal's */
+		/* (You may add extra methods if necessary) */
+		
+		/* ... Your code here ... */
+		
+		
+		/* Determine if the MST by Prim equals the MST by Kruskal */
+		boolean pvk = true;
+		/* ... Your code here ... */
+
+		return pvk;	
+	}
+	
+	  // A utility function to print the constructed MST stored in
+    // parent[]
+    void printMST(int parent[], int n, int graph[][])
+    {
+        System.out.println("Edge   Weight");
+        for (int i = 1; i < V; i++)
+            System.out.println(parent[i]+" - "+ i+"    "+
+                               graph[i][parent[i]]);
+    }
+ 
+    // Function to construct and print MST for a graph represented
+    //  using adjacency matrix representation
+    void primMST(int graph[][])
+    {
+        // Array to store constructed MST
+        int parent[] = new int[V];
+ 
+        // Key values used to pick minimum weight edge in cut
+        int key[] = new int [V];
+ 
+        // To represent set of vertices not yet included in MST
+        Boolean mstSet[] = new Boolean[V];
+ 
+        // Initialize all keys as INFINITE
+        for (int i = 0; i < V; i++)
+        {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+ 
+        // Always include first 1st vertex in MST.
+        key[0] = 0;     // Make key 0 so that this vertex is
+                        // picked as first vertex
+        parent[0] = -1; // First node is always root of MST
+ 
+        // The MST will have V vertices
+        for (int count = 0; count < V-1; count++)
+        {
+            // Pick thd minimum key vertex from the set of vertices
+            // not yet included in MST
+            int u = minKey(key, mstSet);
+ 
+            // Add the picked vertex to the MST Set
+            mstSet[u] = true;
+ 
+            // Update key value and parent index of the adjacent
+            // vertices of the picked vertex. Consider only those
+            // vertices which are not yet included in MST
+            for (int v = 0; v < V; v++)
+ 
+                // graph[u][v] is non zero only for adjacent vertices of m
+                // mstSet[v] is false for vertices not yet included in MST
+                // Update the key only if graph[u][v] is smaller than key[v]
+                if (graph[u][v]!=0 && mstSet[v] == false &&
+                    graph[u][v] <  key[v])
+                {
+                    parent[v]  = u;
+                    key[v] = graph[u][v];
+                }
+        }
+ 
+        // print the constructed MST
+        printMST(parent, V, graph);
+    }
+		
+	/* main()
+	   Contains code to test the PrimVsKruskal function. You may modify the
+	   testing code if needed, but nothing in this function will be considered
+	   during marking, and the testing process used for marking will not
+	   execute any of the code below. 
+	*/
+   public static void main(String[] args) {
+		Scanner s;
+		if (args.length > 0){
+			try{
+				s = new Scanner(new File(args[0]));
+			} catch(java.io.FileNotFoundException e){
+				System.out.printf("Unable to open %s\n",args[0]);
+				return;
+			}
+			System.out.printf("Reading input values from %s.\n",args[0]);
+		}else{
+			s = new Scanner(System.in);
+			System.out.printf("Reading input values from stdin.\n");
+		}
+		
+		int n = s.nextInt();
+		double[][] G = new double[n][n];
+		int valuesRead = 0;
+		for (int i = 0; i < n && s.hasNextDouble(); i++){
+			for (int j = 0; j < n && s.hasNextDouble(); j++){
+				G[i][j] = s.nextDouble();
+				if (i == j && G[i][j] != 0.0) {
+					System.out.printf("Adjacency matrix contains self-loops.\n");
+					return;
+				}
+				if (G[i][j] < 0.0) {
+					System.out.printf("Adjacency matrix contains negative values.\n");
+					return;
+				}
+				if (j < i && G[i][j] != G[j][i]) {
+					System.out.printf("Adjacency matrix is not symmetric.\n");
+					return;
+				}
+				valuesRead++;
+			}
+		}
+		
+		if (valuesRead < n*n){
+			System.out.printf("Adjacency matrix for the graph contains too few values.\n");
+			return;
+		}	
+		
+        boolean pvk = PrimVsKruskal(G);
+        System.out.printf("Does Prim MST = Kruskal MST? %b\n", pvk);
+    }
+}
